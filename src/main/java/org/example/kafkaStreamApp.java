beb32bde4b7488575e6c714b9e09e47f19a3e51b
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-05-12 20:38:59 +0100

    added kafka Stream

---
+++ src/main/java/org/example/kafkaStreamApp.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class kafkaStreamApp {
+
+    public static void main(final String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> textLines = builder.stream("stream-test");
+        //test1
+        textLines.foreach((k, v) -> System.out.println("key: "+k + "<->" + "value: "+ v));
+        //Ktable:
+/*
+        KTable<String, Long> wordCounts = textLines
+                .flatMapValues(textLine -> Arrays.asList(textLine.toLowerCase().split("\\W+")))
+                .groupBy((key, word) -> word)
+                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"));
+        wordCounts.toStream().to("WordsWithCountsTopic", Produced.with(Serdes.String(), Serdes.Long()));
+*/
+        //flatMapValues
+        textLines.flatMapValues((k,v) -> Arrays.asList(v.split(" ")))
+                .foreach((k, v) -> System.out.println("key: "+k + "<->" + "value: "+ v));
+
+        Topology topology = builder.build();
+        //launch the stream
+        KafkaStreams streams = new KafkaStreams(topology, props);
+        streams.start();
+    }
+
+}
